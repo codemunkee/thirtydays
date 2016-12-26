eb1c7ea344450b8b365efa66f773462b813cf956
Russ Nealis <codemunkee@gmail.com>
2016-12-26 09:13:24 -0800

    day five first attempt

--- src/DayFive/Solution.java
+++ src/DayFive/Solution.java
@@ -6,5 +6,10 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            int answer = n * i;
+            System.out.println(n + " x " + i + " = " + answer);
+        }
     }
 }
