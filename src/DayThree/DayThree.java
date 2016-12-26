cc077222e2a3cc43b073f2175f2433d4da281460
Russ Nealis <codemunkee@gmail.com>
2016-12-26 08:52:15 -0800

    first attempt day three

---
+++ src/DayThree/DayThree.java
@@ -0,0 +1,31 @@
+package DayThree;
+
+import java.util.Scanner;
+
+public class DayThree {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        scan.close();
+
+        String ans= "";
+
+        // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+        if (n % 2 == 1) {
+            ans = "Weird";
+        } else {
+            if (n >=2 && n <= 5) {
+                System.out.println("Not Weird");
+            } else if (n >= 6 && n <= 20) {
+                System.out.println("Weird");
+
+            } else {
+                System.out.println("Not Weird");
+            }
+        }
+        System.out.println(ans);
+    }
+}
