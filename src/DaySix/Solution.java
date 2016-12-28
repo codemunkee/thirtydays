4d779c6f2249aa50f1055ba7898d17107314d3e8
Russ Nealis <codemunkee@gmail.com>
2016-12-27 17:18:55 -0800

    day six - boiler plate

---
+++ src/DaySix/Solution.java
@@ -0,0 +1,12 @@
+package DaySix;
+
+/**
+ * Created by rnealis on 12/27/16.
+ */
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
