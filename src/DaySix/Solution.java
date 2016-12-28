00b94260a86f12b34c19dc4ab38474888f6688bb
Russ Nealis <codemunkee@gmail.com>
2016-12-27 17:46:08 -0800

    day six - put chars in arrays

--- src/DaySix/Solution.java
+++ src/DaySix/Solution.java
@@ -1,5 +1,7 @@
 package DaySix;
 
+import java.util.Scanner;
+
 /**
  * Created by rnealis on 12/27/16.
  */
@@ -8,5 +10,30 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+
+        // the number of words we're going to get
+        int T = scan.nextInt();
+
+        String[] words = new String[T];
+
+        for (int i = 0; i < T; i++) {
+            words[i] = scan.next();
+        }
+
+        for (String word: words) {
+            char[] chars = word.toCharArray();
+
+            StringBuilder even = new StringBuilder();
+            StringBuilder odd = new StringBuilder();
+
+            for (int i = 1; i <= chars.length; i++) {
+                if (i % 2 == 0) {
+                    even.append(chars[i]);
+                } else {
+                    odd.append(chars[i]);
+                }
+            }
+        }
     }
 }
