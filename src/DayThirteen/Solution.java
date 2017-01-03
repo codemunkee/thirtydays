8578ff6634fd17d4e550de34eb79581c04b1125d
Russ Nealis <codemunkee@gmail.com>
2017-01-03 10:34:29 -0800

    day thirteen - first stab

--- src/DayThirteen/Solution.java
+++ src/DayThirteen/Solution.java
@@ -13,14 +13,24 @@ abstract class Book
 
 class MyBook extends Book {
 
-    MyBook(String title, String author) {
+    int price;
+
+    MyBook(String title, String author, int price ) {
         super(title, author);
+        this.price = price;
     }
 
     public void display() {
-        System.out.println("Hello World");
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
     }
 }
 
 public class Solution {
+
+    public static void main(String[] args) {
+        MyBook satori = new MyBook("Satori", "Don Winslow", 10);
+        satori.display();
+    }
 }
