9ec319e41a4d8b2ef9ff71f9021a20c58d2dc185
Russ Nealis <codemunkee@gmail.com>
2017-01-04 10:10:54 -0800

    day fourteen - first pass

---
+++ src/DayFourteen/Solution.java
@@ -0,0 +1,47 @@
+package DayFourteen;
+
+import java.util.Scanner;
+
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    Difference(int[] elements) {
+        this.elements = elements;
+    }
+
+    public void computeDifference() {
+        int min, max; min = max = 0;
+
+        for (int i = 0; i < elements.length; i++) {
+
+            if (elements[i] < min) {
+               min = elements[i];
+            }
+            if (elements[i] > max) {
+                max = elements[i];
+            }
+
+            maximumDifference = java.lang.Math.abs(max - min);
+        }
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
