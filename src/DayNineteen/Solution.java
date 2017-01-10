d807e4f9d1911a4a9231de7107b92496f57549dd
Russ Nealis <codemunkee@gmail.com>
2017-01-10 10:31:50 -0800

    day nineteen - forgot to include the integer itself...

--- src/DayNineteen/Solution.java
+++ src/DayNineteen/Solution.java
@@ -9,12 +9,13 @@ interface AdvancedArithmetic{
 class Calculator implements AdvancedArithmetic {
     public int divisorSum(int n) {
         int sum = 0;
-        for (int i = n; i < (n / 2); i++) {
+        for (int i = 2; i <= n; i++) {
             if (n % i == 0) {
                 sum += i;
             }
         }
-        return sum;
+        // we add plus one because every number is divisble by 1
+        return sum + 1;
     }
 }
 
